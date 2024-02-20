2062bfcd4122b4b99ade949b9f83ae2077da64b1
Jenn-2001 <jennifer.soldevilla@students.isatu.edu.ph>
2024-02-20 14:03:20 +0800

    Initial commit

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,26 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Get up to 14% p.a. when you #SwitchToMaya today!
+Instantly earn 12% p.a. on your savings with just ₱500 wallet spend, further boost your interest by referring a friend or use Lazada to pay.
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
---
+++ src/constants/index.js
@@ -0,0 +1,197 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Feedbacks",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Transfer",
+    content:
+      "Instantly send money through QR code or phone number",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "Shop",
+    content:
+      "Whether it be crypto or stocks, you can grow your money how you like it",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Personalized",
+    content:
+      "Shop and withdraw cash with a card that has your @username",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "I am a Paymaya user for over a year now and overall? It's not that bad. Surely, they are top 2 in the finance category (Free, obviously) I've been making transactions with them from the smallest amounts to the 1k range and sometimes they do stutter due to server issues and stuff but one or two refreshes like closing the app's task, you can easily log in without further problems.",
+    name: "Commandment",
+    title: "User",
+    img: people03,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "It is a good app, they also have a website but for FAQ's about their product. When I've transfer an amount from Paymaya to Coins.ph the system accidentally left the amount at their site. Meaning the transaction did not push through to be send to my coins.ph. after 24 hours luckily their customer service made an action the P100 Philippine Peso was received successfully on my coins.ph account.",
+    name: "greatcrafts",
+    title: "User",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "I like it. It's like having everything in 1 app. So convenient!",
+    name: "Jenna",
+    title: "User",
+    img: people01,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "",
+    value: "Instantly send money through QR code or phone number",
+  },
+  {
+    id: "stats-2",
+    title: "",
+    value: "Shop online and in-store from our wide variety of merchants and community of verified sellers",
+  },
+  {
+    id: "stats-3",
+    title: "",
+    value: "Rest easy with your savings tucked away in a BSP regulated bank",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Home",
+        link: "https://www.hoobank.com/content/",
+      },
+      {
+        name: "User Guide",
+        link: "https://www.hoobank.com/how-it-works/",
+      },
+      {
+        name: "Deals",
+        link: "https://www.hoobank.com/create/",
+      },
+      {
+        name: "Store",
+        link: "https://www.hoobank.com/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.hoobank.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.hoobank.com/help-center/",
+      },
+      {
+        name: "Careers",
+        link: "https://www.hoobank.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.hoobank.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.hoobank.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.hoobank.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partners",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
